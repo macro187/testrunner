9f352a4feab16c046ff367b4c04c5880aacbe409
Ron MacNeil <macro@hotmail.com>
2016-09-24 20:41:46 +1000

    Rebuild solution and project, and get rid of unit test projects
    Will build better test projects later.

---
+++ TestRunner/Properties/AssemblyInfo.cs
@@ -0,0 +1,16 @@
+﻿using System.Reflection;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("TestRunner")]
+[assembly: AssemblyDescription("A console MSTest runner")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("TestRunner")]
+[assembly: AssemblyCopyright("Copyright © 2012-2016")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+[assembly: ComVisible(false)]
+[assembly: Guid("3dae3693-dd85-45b9-8a9e-8f99240eee6a")]
+[assembly: AssemblyVersion("0.21.0.0")]
+[assembly: AssemblyFileVersion("0.21.0.0")]
+
