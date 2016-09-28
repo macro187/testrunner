e7f25e548fa7054bbb29bb5c3150f06d771c3835
Ron MacNeil <macro@hotmail.com>
2016-09-29 00:53:48 +1000

    Release v1.1

--- TestRunner/Properties/AssemblyInfo.cs
+++ TestRunner/Properties/AssemblyInfo.cs
@@ -13,5 +13,5 @@ using System.Runtime.InteropServices;
 [assembly: ComVisible(false)]
 [assembly: CLSCompliant(true)]
 [assembly: Guid("3dae3693-dd85-45b9-8a9e-8f99240eee6a")]
-[assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
+[assembly: AssemblyVersion("1.1.0.0")]
+[assembly: AssemblyFileVersion("1.1.0.0")]
