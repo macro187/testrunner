ecf9a561cd1374cc805c4e88eec191103a315567
Ron MacNeil <macro@hotmail.com>
2016-09-28 12:04:59 +1000

    Tests: Don't assume tests run in parallel

--- TestRunner.Tests/TestRunnerTests.cs
+++ TestRunner.Tests/TestRunnerTests.cs
@@ -13,73 +13,65 @@ namespace TestRunner.Tests
     public class TestRunnerTests
     {
 
-        static bool testRunnerDone = false;
-        static string testRunnerOutput = "";
-
-
         [TestMethod]
         public void Run_TestSuite_With_TestRunner()
         {
-            try
+            //
+            // Locate the TestRunner program and the test suite dll
+            //
+            var here = Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location);
+            var testRunner = Path.Combine(here, "TestRunner.exe");
+            var testSuite = Path.Combine(here, "TestRunner.TestSuite.dll");
+
+            //
+            // Run TestRunner against the test suite dll
+            //
+            var args = string.Format("\"{0}\"", testSuite);
+            string output;
+            int exitCode;
+            using (var proc = new Process())
             {
-                var here = Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location);
-                var testRunner = Path.Combine(here, "TestRunner.exe");
-                var testSuite = Path.Combine(here, "TestRunner.TestSuite.dll");
-                var args = string.Format("\"{0}\"", testSuite);
+                bool exited = false;
+                var sb = new StringBuilder();
 
-                using (var proc = new Process())
-                {
-                    bool exited = false;
-                    var output = new StringBuilder();
+                proc.StartInfo.FileName = testRunner;
+                proc.StartInfo.Arguments = args;
+                proc.StartInfo.UseShellExecute = false;
+                proc.StartInfo.RedirectStandardOutput = true;
+                proc.OutputDataReceived += (s,e) => {
+                    Console.WriteLine(e.Data ?? "");
+                    sb.AppendLine(e.Data ?? "");
+                };
+                proc.EnableRaisingEvents = true;
+                proc.Exited += (s,e) => exited = true;
 
-                    proc.StartInfo.FileName = testRunner;
-                    proc.StartInfo.Arguments = args;
-                    proc.StartInfo.UseShellExecute = false;
-                    proc.StartInfo.RedirectStandardOutput = true;
-                    proc.OutputDataReceived += (s,e) => {
-                        Console.WriteLine(e.Data ?? "");
-                        output.AppendLine(e.Data ?? "");
-                    };
-                    proc.EnableRaisingEvents = true;
-                    proc.Exited += (s,e) => exited = true;
+                Console.WriteLine("{0} {1}", testRunner, args);
+                proc.Start();
+                proc.BeginOutputReadLine();
+                while (!exited) Thread.Yield();
 
-                    Console.WriteLine("{0} {1}", testRunner, args);
-                    proc.Start();
-                    proc.BeginOutputReadLine();
-                    while (!exited) Thread.Yield();
-                    testRunnerOutput = output.ToString();
-
-                    Assert.AreEqual(0, proc.ExitCode);
-                }
+                exitCode = proc.ExitCode;
+                output = sb.ToString();
             }
-            finally
-            {
-                testRunnerDone = true;
-            }
-        }
 
+            //
+            // Check stuff
+            //
+            Assert.AreEqual(
+                0, exitCode,
+                "TestRunner.exe returned non-zero exit code");
 
-        [TestMethod]
-        public void TestCleanup_Ran()
-        {
-            while (!testRunnerDone) Thread.Yield();
-            Assert.IsTrue(testRunnerOutput.Contains(TestSuite.TestSuiteTests.TestCleanupMessage));
-        }
+            Assert.IsTrue(
+                output.Contains(TestSuite.TestSuiteTests.TestCleanupMessage),
+                "[TestCleanup] method did not run");
 
+            Assert.IsTrue(
+                output.Contains(TestSuite.TestSuiteTests.ClassCleanupMessage),
+                "[ClassCleanup] method did not run");
 
-        [TestMethod]
-        public void ClassCleanup_Ran()
-        {
-            while (!testRunnerDone) Thread.Yield();
-            Assert.IsTrue(testRunnerOutput.Contains(TestSuite.TestSuiteTests.ClassCleanupMessage));
-        }
-
-
-        [TestMethod]
-        public void AssemblyCleanup_Ran()
-        {
-            while (!testRunnerDone) Thread.Yield();
-            Assert.IsTrue(testRunnerOutput.Contains(TestSuite.TestSuiteTests.AssemblyCleanupMessage));
+            Assert.IsTrue(
+                output.Contains(TestSuite.TestSuiteTests.AssemblyCleanupMessage),
+                "[AssemblyCleanup] method did not run");
         }
 
     }
