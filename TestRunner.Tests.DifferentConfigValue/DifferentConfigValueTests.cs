c437d4b1859ede192145780b7c64d4ce5172ee41
Ron MacNeil <macro@hotmail.com>
2017-09-04 08:25:10 +1000

    Mono 5 no longer supports __MonoCS__

--- TestRunner.Tests.DifferentConfigValue/DifferentConfigValueTests.cs
+++ TestRunner.Tests.DifferentConfigValue/DifferentConfigValueTests.cs
@@ -1,4 +1,5 @@
-﻿using System.Configuration;
+﻿using System;
+using System.Configuration;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 
 namespace TestRunner.Tests.DifferentConfigValue
@@ -8,16 +9,14 @@ namespace TestRunner.Tests.DifferentConfigValue
     public class DifferentConfigValueTests
     {
 
-        #if __MonoCS__
-        //
-        // Config file switching doesn't work on Mono
-        // See https://bugzilla.xamarin.com/show_bug.cgi?id=15741
-        //
-        [Ignore]
-        #endif
         [TestMethod]
         public void TestAssembly_Config_File_Is_Used()
         {
+            //
+            // Config file switching doesn't work on Mono
+            // See https://bugzilla.xamarin.com/show_bug.cgi?id=15741
+            //
+            if (Type.GetType("Mono.Runtime") != null) return;
             Assert.AreEqual(
                 "DifferentConfigFileValue",
                 ConfigurationManager.AppSettings["ConfigFileKey"]);
--- TestRunner.Tests.MSTest/MSTestTests.cs
+++ TestRunner.Tests.MSTest/MSTestTests.cs
@@ -90,16 +90,14 @@ namespace TestRunner.Tests.MSTest
         }
 
 
-        #if __MonoCS__
-        //
-        // Config file switching doesn't work on Mono
-        // See https://bugzilla.xamarin.com/show_bug.cgi?id=15741
-        //
-        [Ignore]
-        #endif
         [TestMethod]
         public void TestAssembly_Config_File_Is_Used()
         {
+            //
+            // Config file switching doesn't work on Mono
+            // See https://bugzilla.xamarin.com/show_bug.cgi?id=15741
+            //
+            if (Type.GetType("Mono.Runtime") != null) return;
             Assert.AreEqual(
                 "ConfigFileValue",
                 ConfigurationManager.AppSettings["ConfigFileKey"]);
