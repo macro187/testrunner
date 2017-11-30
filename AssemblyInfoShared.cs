6fb778fc627367fe66c161d98605356029e0cb92
Ron MacNeil <macro@hotmail.com>
2017-11-30 22:14:20 +1000

    Release version 1.5.1

--- AssemblyInfoShared.cs
+++ AssemblyInfoShared.cs
@@ -6,7 +6,7 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyDescription("A console MSTest runner")]
 [assembly: AssemblyCompany("Ron MacNeil")]
 [assembly: AssemblyCopyright("Copyright (c) 2012-2017 Ron MacNeil, Rickenberg")]
-[assembly: AssemblyInformationalVersion("1.5.1-master")]
+[assembly: AssemblyInformationalVersion("1.5.1")]
 [assembly: AssemblyFileVersion("1.5.1.0")]
 [assembly: AssemblyVersion("1.0.0.0")]
 [assembly: ComVisible(false)]
