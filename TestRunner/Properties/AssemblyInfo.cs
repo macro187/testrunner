733561295b62693edcce765134f2f36be99b3f76
Ron MacNeil <macro@hotmail.com>
2017-11-30 21:53:32 +1000

    Tidy up AssemblyInfos

--- TestRunner/Properties/AssemblyInfo.cs
+++ TestRunner/Properties/AssemblyInfo.cs
@@ -1,17 +1,3 @@
-﻿using System;
 using System.Reflection;
-using System.Runtime.InteropServices;
 
-[assembly: AssemblyProduct("TestRunner")]
-[assembly: AssemblyTitle("A console MSTest runner")]
-[assembly: AssemblyDescription("")]
-[assembly: AssemblyCopyright("Copyright (c) 2012-2017")]
-[assembly: AssemblyCompany("Ron MacNeil, Rickenberg")]
-[assembly: AssemblyConfiguration("")]
-[assembly: AssemblyTrademark("")]
-[assembly: AssemblyCulture("")]
-[assembly: ComVisible(false)]
-[assembly: CLSCompliant(true)]
-[assembly: Guid("3dae3693-dd85-45b9-8a9e-8f99240eee6a")]
-[assembly: AssemblyVersion("1.5.0.0")]
-[assembly: AssemblyFileVersion("1.5.0.0")]
+[assembly: AssemblyTitle("TestRunner")]
