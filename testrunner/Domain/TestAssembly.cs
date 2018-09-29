ad1429099947c384bfc9102e6e6e696c1e4f4643
Ron MacNeil <macro@hotmail.com>
2018-09-29 19:01:42 +1000

    Remove ConsoleExtensions which are no longer required

--- testrunner/Domain/TestAssembly.cs
+++ testrunner/Domain/TestAssembly.cs
@@ -1,10 +1,8 @@
-﻿using System;
-using System.Collections.Generic;
+﻿using System.Collections.Generic;
 using System.Collections.ObjectModel;
 using System.Linq;
 using System.Reflection;
 using TestRunner.Infrastructure;
-using static TestRunner.Infrastructure.ConsoleExtensions;
 
 namespace TestRunner.Domain
 {
