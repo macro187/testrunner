44eeda07c53d3ddf23dac226b42187f45871a4f7
Ron MacNeil <macro@hotmail.com>
2018-10-01 12:27:58 +1000

    Move ExceptionExtensions into DefaultOutputEventHandler

--- testrunner/Events/DefaultOutputEventHandler.cs
+++ testrunner/Events/DefaultOutputEventHandler.cs
@@ -1,6 +1,8 @@
 ﻿using System;
+using System.Collections;
 using System.Linq;
 using System.Reflection;
+using System.Text;
 using TestRunner.Infrastructure;
 
 namespace TestRunner.Events
@@ -93,6 +95,57 @@ namespace TestRunner.Events
         }
 
 
+        static string FormatException(Exception ex)
+        {
+            if (ex == null) return "";
+            var sb = new StringBuilder();
+            sb.AppendLine(ex.Message);
+            sb.AppendLine("Type: " + ex.GetType().FullName);
+            if (ex.Data != null)
+            {
+                foreach (DictionaryEntry de in ex.Data)
+                {
+                    sb.AppendLine($"Data.{de.Key}: {de.Value}");
+                }
+            }
+            if (!string.IsNullOrWhiteSpace(ex.Source))
+            {
+                sb.AppendLine("Source: " + ex.Source);
+            }
+            if (!string.IsNullOrWhiteSpace(ex.HelpLink))
+            {
+                sb.AppendLine("HelpLink: " + ex.HelpLink);
+            }
+            if (!string.IsNullOrWhiteSpace(ex.StackTrace))
+            {
+                sb.AppendLine("StackTrace:");
+                sb.AppendLine(StringExtensions.Indent(FormatStackTrace(ex.StackTrace)));
+            }
+            if (ex.InnerException != null)
+            {
+                sb.AppendLine("InnerException:");
+                sb.AppendLine(StringExtensions.Indent(FormatException(ex.InnerException)));
+            }
+            return sb.ToString();
+        }
+
+
+        static string FormatStackTrace(string stackTrace)
+        {
+            return string.Join(
+                Environment.NewLine,
+                StringExtensions.SplitLines(stackTrace)
+                    .Select(line => line.Trim())
+                    .SelectMany(line => {
+                        var i = line.IndexOf(" in ", StringComparison.Ordinal);
+                        if (i <= 0) return new[] {line};
+                        var inPart = line.Substring(i + 1);
+                        var atPart = line.Substring(0, i);
+                        return new[] {atPart, StringExtensions.Indent(inPart)};
+                        }));
+        }
+
+
         public override void ProgramBannerEvent(params string[] lines)
         {
             WriteError();
@@ -131,11 +184,11 @@ namespace TestRunner.Events
             {
                 foreach (var le in rtle.LoaderExceptions)
                 {
-                    WriteError(ExceptionExtensions.FormatException(le));
+                    WriteError(FormatException(le));
                 }
             }
 
-            WriteError(ExceptionExtensions.FormatException(exception));
+            WriteError(FormatException(exception));
 
             base.ProgramInternalErrorEvent(exception);
         }
@@ -144,7 +197,6 @@ namespace TestRunner.Events
         public override void TestAssemblyBeginEvent(string path)
         {
             Guard.NotNull(path, nameof(path));
-            WriteOut();
             WriteOut();
             WriteHeadingOut(path);
             base.TestAssemblyBeginEvent(path);
@@ -415,7 +467,7 @@ namespace TestRunner.Events
             Guard.NotNull(expected, nameof(expected));
             Guard.NotNull(exception, nameof(exception));
             WriteOut($"  [ExpectedException] {expected.FullName} occurred:");
-            WriteOut(StringExtensions.Indent(ExceptionExtensions.FormatException(exception)));
+            WriteOut(StringExtensions.Indent(FormatException(exception)));
             base.MethodExpectedExceptionEvent(expected, exception);
         }
 
@@ -423,7 +475,7 @@ namespace TestRunner.Events
         public override void MethodUnexpectedExceptionEvent(Exception exception)
         {
             Guard.NotNull(exception, nameof(exception));
-            WriteOut(StringExtensions.Indent(ExceptionExtensions.FormatException(exception)));
+            WriteOut(StringExtensions.Indent(FormatException(exception)));
             base.MethodUnexpectedExceptionEvent(exception);
         }
 
