156d67a9cffb8d6c89a7c1d5f01cb78f603ca2c5
Ron MacNeil <macro@hotmail.com>
2018-10-08 18:28:05 +1000

    Simplify machine output format
    By including pid and timestamp in TestRunnerEvent.

--- testrunner/Events/MachineOutputEventHandler.cs
+++ testrunner/Events/MachineOutputEventHandler.cs
@@ -1,11 +1,9 @@
 ﻿using System;
-using System.Diagnostics;
 using System.IO;
 using System.Runtime.Serialization;
 using System.Runtime.Serialization.Json;
 using System.Text;
 using TestRunner.Domain;
-using TestRunner.Infrastructure;
 
 namespace TestRunner.Events
 {
@@ -17,75 +15,70 @@ namespace TestRunner.Events
     public class MachineOutputEventHandler : EventHandler
     {
 
-        static void WriteOut(string message = "")
-        {
-            message = message ?? "";
-            foreach (var line in StringExtensions.SplitLines(message)) Console.Out.WriteLine(line);
-        }
+        readonly DataContractJsonSerializer Serializer = new DataContractJsonSerializer(
+            typeof(TestRunnerEvent),
+            new DataContractJsonSerializerSettings() {
+                EmitTypeInformation = EmitTypeInformation.Never,
+                DateTimeFormat = new DateTimeFormat("o"), // ISO8601
+                KnownTypes = new[]{
+                    typeof(UnitTestOutcome),
+                    typeof(ExceptionInfo),
+                    typeof(StackFrameInfo),
+                    typeof(TestRunnerEvent),
+                    typeof(ProgramBannerEvent),
+                    typeof(ProgramUsageEvent),
+                    typeof(ProgramUserErrorEvent),
+                    typeof(ProgramInternalErrorEvent),
+                    typeof(TestAssemblyBeginEvent),
+                    typeof(TestAssemblyNotFoundEvent),
+                    typeof(TestAssemblyNotDotNetEvent),
+                    typeof(TestAssemblyNotTestEvent),
+                    typeof(TestAssemblyConfigFileSwitchedEvent),
+                    typeof(TestAssemblyEndEvent),
+                    typeof(TestClassBeginEvent),
+                    typeof(TestClassEndEvent),
+                    typeof(TestBeginEvent),
+                    typeof(TestIgnoredEvent),
+                    typeof(TestEndEvent),
+                    typeof(AssemblyInitializeMethodBeginEvent),
+                    typeof(AssemblyInitializeMethodEndEvent),
+                    typeof(AssemblyCleanupMethodBeginEvent),
+                    typeof(AssemblyCleanupMethodEndEvent),
+                    typeof(ClassInitializeMethodBeginEvent),
+                    typeof(ClassInitializeMethodEndEvent),
+                    typeof(ClassCleanupMethodBeginEvent),
+                    typeof(ClassCleanupMethodEndEvent),
+                    typeof(TestContextSetterBeginEvent),
+                    typeof(TestContextSetterEndEvent),
+                    typeof(TestInitializeMethodBeginEvent),
+                    typeof(TestInitializeMethodEndEvent),
+                    typeof(TestMethodBeginEvent),
+                    typeof(TestMethodEndEvent),
+                    typeof(TestCleanupMethodBeginEvent),
+                    typeof(TestCleanupMethodEndEvent),
+                    typeof(MethodExpectedExceptionEvent),
+                    typeof(MethodUnexpectedExceptionEvent),
+                    typeof(OutputTraceEvent),
+                },
+            });
 
 
         public override void Handle(TestRunnerEvent e)
         {
-            var serializer = new DataContractJsonSerializer(
-                typeof(TestRunnerEvent),
-                new DataContractJsonSerializerSettings() {
-                    DateTimeFormat = new DateTimeFormat("o"), // ISO8601
-                    EmitTypeInformation = EmitTypeInformation.Never,
-                    KnownTypes = new[]{
-                        typeof(UnitTestOutcome),
-                        typeof(ExceptionInfo),
-                        typeof(StackFrameInfo),
-                        typeof(TestRunnerEvent),
-                        typeof(ProgramBannerEvent),
-                        typeof(ProgramUsageEvent),
-                        typeof(ProgramUserErrorEvent),
-                        typeof(ProgramInternalErrorEvent),
-                        typeof(TestAssemblyBeginEvent),
-                        typeof(TestAssemblyNotFoundEvent),
-                        typeof(TestAssemblyNotDotNetEvent),
-                        typeof(TestAssemblyNotTestEvent),
-                        typeof(TestAssemblyConfigFileSwitchedEvent),
-                        typeof(TestAssemblyEndEvent),
-                        typeof(TestClassBeginEvent),
-                        typeof(TestClassEndEvent),
-                        typeof(TestBeginEvent),
-                        typeof(TestIgnoredEvent),
-                        typeof(TestEndEvent),
-                        typeof(AssemblyInitializeMethodBeginEvent),
-                        typeof(AssemblyInitializeMethodEndEvent),
-                        typeof(AssemblyCleanupMethodBeginEvent),
-                        typeof(AssemblyCleanupMethodEndEvent),
-                        typeof(ClassInitializeMethodBeginEvent),
-                        typeof(ClassInitializeMethodEndEvent),
-                        typeof(ClassCleanupMethodBeginEvent),
-                        typeof(ClassCleanupMethodEndEvent),
-                        typeof(TestContextSetterBeginEvent),
-                        typeof(TestContextSetterEndEvent),
-                        typeof(TestInitializeMethodBeginEvent),
-                        typeof(TestInitializeMethodEndEvent),
-                        typeof(TestMethodBeginEvent),
-                        typeof(TestMethodEndEvent),
-                        typeof(TestCleanupMethodBeginEvent),
-                        typeof(TestCleanupMethodEndEvent),
-                        typeof(MethodExpectedExceptionEvent),
-                        typeof(MethodUnexpectedExceptionEvent),
-                        typeof(OutputTraceEvent),
-                    },
-                });
+            var name = e.GetType().Name;
+            var json = Serialize(e);
+            Console.Out.WriteLine($"{name} {json}");
+            base.Handle(e);
+        }
 
-            string json;
+
+        string Serialize(TestRunnerEvent e)
+        {
             using (var stream = new MemoryStream())
             {
-                serializer.WriteObject(stream, e);
-                json = Encoding.UTF8.GetString(stream.GetBuffer(), 0, (int)stream.Position);
+                Serializer.WriteObject(stream, e);
+                return Encoding.UTF8.GetString(stream.GetBuffer(), 0, (int)stream.Position);
             }
-
-            var name = e.GetType().Name;
-            var pid = Process.GetCurrentProcess().Id;
-            var prefix = $"[{name}:{pid}]";
-            WriteOut($"{prefix} {json}");
-            
-            base.Handle(e);
         }
 
     }
--- testrunner/Events/TestRunnerEvent.cs
+++ testrunner/Events/TestRunnerEvent.cs
@@ -1,6 +1,20 @@
-﻿namespace TestRunner.Events
+﻿using System;
+using System.Diagnostics;
+
+namespace TestRunner.Events
 {
     public abstract class TestRunnerEvent
     {
+
+        public TestRunnerEvent()
+        {
+            ProcessId = Process.GetCurrentProcess().Id;
+            Timestamp = DateTime.Now;
+        }
+
+
+        public int ProcessId { get; set; }
+        public DateTime Timestamp { get; set; }
+
     }
 }
